bd0da941519de05b5028535b3eac018eeefab2c2
Daulet6080 <112867425+Daulet6080@users.noreply.github.com>
2024-04-10 20:23:47 +0600

    Add files via upload

---
+++ Lab9/hhback/api/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Company, Vacancy
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'address')
+    search_fields = ('name', 'city')
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'salary', 'company')
+    list_filter = ('company', 'salary')
+    search_fields = ('name', 'description')
+
+
+
+# Register your models here.
